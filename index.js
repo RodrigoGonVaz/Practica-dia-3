1301fcabada7cbe0e74164f9fd9a6153c05cf7c4
Rodrigo Gonzalez <rodrigogonvaz@gmail.com>
2021-11-17 09:46:20 -0600

    Gestion de carpeta partials

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //1. IMPORTACIONES
 const express       = require("express")
-
 const app           = express()
+const hbs           = require("hbs")
 
 require("dotenv").config()
 
@@ -10,6 +10,9 @@ app.use(express.static('public'))
 
 app.set("views", __dirname + "/views")
 app.set("view engine", "hbs")
+
+hbs.registerPartials(__dirname + "/views/partials") //activar los partials
+
 
 
 //3. RUTAS
